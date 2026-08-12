be8432c346eebe71f4f198d53c3ad26651b720ac
Rishi Challa <challarishi02@gmail.com>
2026-08-12 17:09:10 +0530

    env changes

--- server/server.js
+++ server/server.js
@@ -46,6 +46,6 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(
-    `Server running on port ${PORT} [${process.env.NODE_ENV || "development"}]`,
+    `Server running on port ${PORT} [${process.env.NODE_ENV || "production"}]`,
   ),
 );
