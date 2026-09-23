b694c4d5e37f7cc48f84c079ac111c9b281b96a0
Rishi Challa <challarishi02@gmail.com>
2026-09-23 16:09:11 +0530

    MyBid ads minor changes

--- src/components/MyBidAds.jsx
+++ src/components/MyBidAds.jsx
@@ -28,7 +28,7 @@ export default function AAdsBanner() {
   return (
     <div
       ref={containerRef}
-      className="w-full flex justify-center items-center my-4 min-h-[70px] overflow-hidden"
+    //   className="w-full flex justify-center items-center my-2 min-h-[20px] overflow-hidden"
     />
   );
 }
